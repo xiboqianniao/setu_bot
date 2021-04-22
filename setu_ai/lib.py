2379a58a2c49771b4d84465c206216dc88582c68
蓝红心 <43800204+LHXnois@users.noreply.github.com>
2021-04-22 23:25:45 +0800

    第一次提交

---
+++ setu_ai/lib.py
@@ -0,0 +1,346 @@
+from hoshino import R, aiorequests
+from hoshino.typing import Union
+from hoshino.Gm import Gm
+from urllib.parse import unquote
+from random import choice
+import time
+score_class = {
+        '0': 0,
+        '1': 1,
+        '2': 2,
+        '4': 3,
+        '8': 4,
+        '16': 5,
+        '32': 6,
+        '64': 7}
+
+
+class Config():
+    data = R.data('setu_test/config.json', 'json')
+    if not data.exist:
+        data.write({
+            'host': r'http://saki.fumiama.top/',
+            'online': True,
+            'key': '关婴',
+            'sign': 'signup',
+            'get': 'pick',
+            'score': 'vote',
+        })
+    config = data.read
+
+    @property
+    def host(self):
+        return self.config['host']
+
+    @property
+    def online(self):
+        return self.config['online']
+
+    @property
+    def key(self):
+        return int.from_bytes(
+            self.config['key'].encode("utf-16-be"), byteorder='big')
+
+    def value(self, key):
+        return self.config[key]
+
+    def set(self, name, value):
+        self.config[name] = value
+        self.data.write(self.config)
+
+    @property
+    def signurl(self):
+        signkey = self.key ^ int(time.time())
+        return self.host+self.value('sign')+'?{:0>10d}'.format(signkey)
+
+    def pickurl(self, name):
+        return self.host+self.value('get')+'?'+name
+
+    def scoreurl(self, name, img, score):
+        img = img.split('.')[0]
+        return self.host+self.value(
+            'score')+f'?uuid={name}&img={img}&class={score}'
+
+
+class User():
+    data = R.data('setu_test/user.json', 'json')
+    if not data.exist:
+        data.write({'3036995449': {
+            'name': '弲法',
+            'gcount': 0,
+            'scount': 0,
+            'ucount': 0,
+            'uimg': [],
+            'simg': {
+                'online': [],
+                'offline': []
+        }}})
+    user = data.read
+
+    def __init__(self, uid: int) -> None:
+        self.uid = str(uid)
+        if self.uid not in self.user:
+            self.user[self.uid] = {
+                'name': '',
+                'gcount': 0,
+                'scount': 0,
+                'ucount': 0,
+                'uimg': [],
+                'simg': {
+                    'online': [],
+                    'offline': []
+                }}
+            self.save
+        self.name = self.user[self.uid]['name']
+
+    def count(self, ctype: str) -> int:
+        if ctype in ['get']:
+            ctype = 'g'
+        elif ctype in ['score']:
+            ctype = 's'
+        elif ctype in ['upload']:
+            ctype = 'u'
+        return self.user[self.uid][ctype+'count']
+
+    @property
+    def save(self):
+        self.data.write(self.user)
+
+    @property
+    async def getname(self) -> str:
+        if self.name == '':
+            resp = await aiorequests.get(Config().signurl)
+            self.name = await resp.text
+            self.user[self.uid]['name'] = self.name
+            self.save
+        return self.name
+
+    def addcount(self, ctype: str, num: int = 1) -> None:
+        if ctype in ['get']:
+            ctype = 'g'
+        elif ctype in ['score']:
+            ctype = 's'
+        elif ctype in ['upload']:
+            ctype = 'u'
+        self.user[self.uid][ctype+'count'] += num
+        self.save
+
+    def addscinfo(self, img: str, ptype: str = 'on') -> None:
+        if img not in self.user[self.uid]['simg'][ptype+'line']:
+            self.addcount('s')
+            self.user[self.uid]['simg'][ptype+'line'].append(img)
+            self.save
+            return True
+        return False
+
+    def addupinfo(self, img: str):
+        self.user[self.uid]['uimg'].append(img)
+        self.save
+
+    @property
+    def get_scored_img(self):
+        return self.user[self.uid]['simg']['offline']
+
+    @property
+    async def nickname(self) -> str:
+        if 'nickname' not in self.user[self.uid]:
+            self.user[self.uid]['nickname'] = await Gm.user_info(
+                                                        int(self.uid), 'nickname')
+            self.save
+        return self.user[self.uid]['nickname']
+
+    @property
+    def avator(self):
+        return f'http://q1.qlogo.cn/g?b=qq&nk={self.uid}&s=1'
+
+
+class Rec():
+    data = R.data('setu_test/rec.json', 'json')
+    if not data.exist:
+        cont = {'data': {}, 'tem': []}
+        data.write(cont)
+    rec = data.read
+    maxrec = 250
+    popn = 50
+
+    @classmethod
+    def save(self):
+        self.data.write(self.rec)
+
+    @classmethod
+    def add(self,
+            msgid: int,
+            img,
+            online: bool = True
+            ) -> None:
+        self.rel()
+        if isinstance(img, str):
+            img = (img, )
+        self.rec['data'][str(msgid)] = {'img': img, 'online': online}
+        self.rec['tem'].append(str(msgid))
+        self.save()
+
+    @classmethod
+    def rel(self):
+        if len(self.rec['tem']) > self.maxrec:
+            list(map(self.rec['data'].pop, self.rec['tem'][:self.popn]))
+            self.rec['tem'] = self.rec['tem'][self.popn:]
+            self.save()
+
+    @classmethod
+    def get(self, msgid: int) -> dict:
+        return self.rec['data'][str(msgid)] if str(msgid) in self.rec['data'] else False
+
+    @classmethod
+    def clone(self, msgid: int, oid: int):
+        cache = self.rec['data'][str(oid)]
+        self.add(msgid, cache['img'], cache['online'])
+
+
+class Pic():
+    score_class = {
+        '0': 0,
+        '1': 1,
+        '2': 2,
+        '4': 3,
+        '8': 4,
+        '16': 5,
+        '32': 6,
+        '64': 7}
+    data = R.data('setu_test/data.json', 'json')
+    if not data.exist:
+        data.write({
+            'online': 0,
+            'offline': 0,
+            'onlinepic': {},
+            'offlinepic': {}
+        })
+    pics = data.read
+
+    @classmethod
+    def save(self):
+        self.data.write(self.pics)
+
+    @classmethod
+    def addpic(self, img: str, ptype: bool = 'on', **info) -> bool:
+        if img not in self.pics[ptype+'linepic']:
+            self.pics[ptype+'line'] += 1
+            info['score'] = {}
+            self.pics[ptype+'linepic'][img] = info
+            self.save()
+            return True
+        return False
+
+    @classmethod
+    async def scorepic(
+            self, uid: int, score: int, img: str, ptype: str = 'on') -> str:
+        user = User(uid)
+        user.addscinfo(img, ptype)
+        self.pics[ptype+'linepic'][img]['score'][user.uid] = score
+        self.save()
+        if ptype == 'on' and Config().online:
+            name = await user.getname
+            url = Config().scoreurl(name, img, score)
+            resp = await aiorequests.get(url)
+            resp = await resp.text
+            return resp
+        else:
+            return 'succ'
+
+    @classmethod
+    async def getpic(self, uid: int, online: bool = True) -> tuple:
+        user = User(uid)
+        if self.pics['offline'] < 10:
+            online = True
+        if len(user.get_scored_img) > self.pics['offline']:
+            online = True
+        if online and Config().online:
+            url = Config().pickurl(await user.getname)
+            resp = await aiorequests.get(url)
+            img = await resp.text
+            imgname = f'{unquote(img)}.webp'
+            pic = R.tem_img('setu_test/online', imgname)
+            if self.addpic(imgname):
+                await pic.download(Config().host+img)
+            source = 'powered by '+Config().host
+        else:
+            allpic = set(self.pics['offlinepic'].keys())
+            unspic = allpic.difference(set(user.get_scored_img))
+            if not unspic:
+                return
+            imgname = choice(list(unspic))
+            pic = R.tem_img('setu_test/upload', imgname)
+            upid = self.pics['offlinepic'][imgname]['uid']
+            nickname = await User(upid).nickname
+            source = f'uploader: {nickname}({upid})'
+        user.addcount('g')
+        pic = f'{pic.cqcode}\n{source}'
+        return (pic, (imgname, online))
+
+    @classmethod
+    async def uppic(self, uid: int, msgid: int, imginfo: dict):
+        user = User(uid)
+        upcount = 0
+        upedimg = []
+        for imgname in imginfo:
+            img = await R.tem_img(
+                'setu_test/upload', imgname).download(imginfo[imgname])
+            imgname = img.path.split('\\')[-1]
+            upedimg.append(imgname)
+            # files = {'img': open(img.path, 'rb')}
+            # url = host + 'upform'
+            # resp = await aiorequests.post(url, files=files)
+            # resp = await resp.text
+            # 怎么就不得行呢
+            if self.addpic(imgname, 'off',
+                           uid=uid,
+                           uptime=time.asctime()):
+                upcount += 1
+                user.addupinfo(imgname)
+        user.addcount('u', upcount)
+        Rec.add(msgid, tuple(upedimg), False)
+        return upcount
+
+
+class PicListener:
+    def __init__(self):
+        self.on = {}
+        self.rec = {}
+
+    def get_on_off_status(self, gid, uid):
+        if gid not in self.on:
+            self.on[gid] = []
+            return False
+        else:
+            return uid in self.on[gid]
+
+    def turn_on(self, gid, uid):
+        if gid not in self.on:
+            self.on[gid] = [uid]
+        else:
+            self.on[gid].append(uid)
+
+    def turn_off(self, gid, uid):
+        if gid not in self.on:
+            self.on[gid] = []
+        elif uid in self.on[gid]:
+            self.on[gid].remove(uid)
+
+    def arec(self, uid, img, online):
+        online = 'on' if online else 'off'
+        self.rec[uid] = (img, online)
+
+    def getrec(self, uid):
+        return self.rec[uid] if uid in self.rec else None
+
+
+async def scoreall(uid: int, msgid: int, score: list) -> Union[tuple, list]:
+    imgs = Rec.get(msgid)
+    if len(imgs['img']) == len(score):
+        ptype = 'on' if imgs['online'] else 'off'
+        resp = []
+        for img, s in zip(imgs['img'], score):
+            resp.append(
+                await Pic.scorepic(uid, Pic.score_class[s], img, ptype))
+        return resp if len(resp) > 0 else [False]
+    return False, len(imgs['img']), len(score)
---
+++ setu_ai/setu_test.py
@@ -0,0 +1,196 @@
+import hoshino
+from hoshino import Service, priv
+from hoshino.typing import CQEvent
+from hoshino.util import FreqLimiter
+from .lib import User, Pic, Rec, scoreall, Config, PicListener, choice, Gm
+HELP = '''自助评分终端机
+[涩图] 叫一份涩图
+[<回复图片> 打分x] 为涩图打分
+ps：只支持打0，1，2，4，8，16，32，64分
+[上传图片] 附带图片即可上传，如果不带图片则开启上传模式
+ps：上传模式中发送的图片都会上传
+[网页端] 获得网页端地址
+[涩批信息] 获得个人一些简单的统计信息
+====
+私聊bot：
+[开始评分] 进入高效打分模式
+高效打分模式下直接回复分数，打分后立刻发送下一张图
+[结束评分] 结束高效打分模式
+'''
+sv = Service('setu_ai', bundle='setu', help_=HELP)
+
+_freq = FreqLimiter(2.5)
+pls = PicListener()
+Config = Config()
+
+
+@sv.on_rex(r'^(([色涩瑟]图)|setu)|[来发给]((?P<num>\d+)|(?:.*))[张个幅点份丶][色涩瑟]图?$')
+async def checkswitch(bot, ev: CQEvent):
+    uid = ev.user_id
+    if not _freq.check(uid):
+        await bot.finish(ev, '您冲得太快了，请慢一点💦')
+    _freq.start_cd(uid)
+    num = ev['match'].group('num')
+    if num:
+        try:
+            num = int(num)
+            max_num = 5
+            if num > max_num:
+                await bot.send(ev, f'太贪心辣,一次只能要{max_num}份涩图哦~')
+                num = max_num
+            else:
+                pass
+        except:
+            num = 1
+    else:
+        num = 1
+    for _ in range(num):
+        online = choice((True, False))
+        if (pic := await Pic.getpic(uid, online)) is None:
+            await bot.finish(ev, '已经没有更多图片了！')
+        msginfo = await bot.send(ev, pic[0])
+        Rec.add(msginfo['message_id'], *pic[1])
+
+
+@sv.on_replay(startwith='打分')
+async def testrep(bot, ev: CQEvent):
+    msgid = ev['quote_message']['message_id']
+    if not Rec.get(msgid):
+        return
+    uid = ev.user_id
+    if not _freq.check(uid):
+        await bot.finish(ev, '您冲得太快了，请慢一点💦')
+    _freq.start_cd(uid)
+    kw = ev['rep_message'].extract_plain_text().strip().split()
+    for i in kw:
+        if i not in Pic.score_class:
+            await bot.finish(ev, '💦只支持评价0, 1, 2, 4, 8, 16, 32, 64分哦')
+    resp = await scoreall(uid, msgid, kw)
+    if not resp[0]:
+        await bot.finish(ev, f'{resp[1]}张图片只收到了{resp[2]}个评分')
+    msg = []
+    count = 0
+    for i, res in enumerate(resp):
+        if res == 'succ':
+            count += 1
+        else:
+            msg.append(f'{f"第{i+1}张图片" if len(resp) > 1 else ""}评价失败惹...{res}')
+    if count:
+        msg.append(f'{f"{count}张图片" if count > 1 else ""}评价成功！感谢反馈')
+    Rec.clone(ev.message_id, msgid)
+    await bot.send(ev, '\n'.join(msg))
+
+
+@sv.on_suffix(('上传图片', '上传'))
+@sv.on_prefix(('上传图片', '上传'))
+async def upload(bot, ev: CQEvent):
+    imginfo = {}
+    uid = ev.user_id
+    for i in ev.message:
+        if i['type'] == 'image':
+            imginfo[i['data']['file']] = i['data']['url']
+    if imginfo:
+        count = await Pic.uppic(uid, ev.message_id, imginfo)
+        a = '张' if count < 2 else '些'
+        await bot.finish(ev, f'成功上传{count}张图片, 现在可以回复这{a}图片打分了')
+    else:
+        gid = ev.group_id
+        kw = ev.message.extract_plain_text().strip()
+        if pls.get_on_off_status(gid, uid):
+            if kw in ('关闭', '停止', '结束'):
+                pls.turn_off(gid, uid)
+                await bot.finish(ev, "上传模式已关闭！💤")
+            await bot.finish(ev, "您已经在上传模式下啦！\n如想退出上传模式请发送“结束上传”~")
+        elif kw == '':
+            pls.turn_on(gid, uid)
+            await bot.send(ev, "✨了解～请发送图片吧！支持批量噢！\n如想退出上传模式请发送“结束上传”~")
+
+
+@sv.on_message('group')
+async def picmessage(bot, ev: CQEvent):
+    uid = ev.user_id
+    if not pls.get_on_off_status(ev.group_id, uid):
+        return
+    imginfo = {}
+    for i in ev.message:
+        if i['type'] == 'image':
+            imginfo[i['data']['file']] = i['data']['url']
+    if imginfo:
+        count = await Pic.uppic(uid, ev.message_id, imginfo)
+        a = '张' if count < 2 else '些'
+        await bot.send(ev, f'成功上传{count}张图片🎈, 现在可以回复这{a}图片打分了')
+
+hbot = hoshino.get_bot()
+@hbot.on_message('private')
+async def picmessagep(ev):
+    uid = ev.user_id
+    if str(ev.message) in ('开始评分', '开始'):
+        pls.turn_on(0, uid)
+        await hbot.send(ev, '🎉无情评价机器启动！私聊模式直接回复分数，打分后立刻下一张图\n结束打分请发送“结束”')
+    elif str(ev.message) in ('结束', '结束评分'):
+        pls.turn_off(0, uid)
+        pls.rec[uid] = ()
+        await hbot.send(ev, '已结束💤')
+    if not pls.get_on_off_status(0, uid):
+        return
+    if info := pls.getrec(uid):
+        msg = str(ev.message).strip().strip('打分').strip('分')
+        if msg in Pic.score_class:
+            resp = await Pic.scorepic(
+                uid, Pic.score_class[msg], info[0], info[1])
+            if resp == 'succ':
+                await hbot.send(ev, '评价成功！感谢反馈')
+            else:
+                await hbot.send(ev, f'评价失败惹...{resp}')
+        else:
+            await hbot.finish(ev, '💦只支持评价0, 1, 2, 4, 8, 16, 32, 64分哦')
+    online = choice((True, False))
+    await hbot.send(ev, '💦正在扒拉图片...')
+    if (pic := await Pic.getpic(uid, online)) is None:
+        await hbot.finish(ev, '已经没有更多图片了！')
+        pls.turn_off(0, uid)
+    await hbot.send(ev, pic[0])
+    pls.arec(uid, *pic[1])
+
+@sv.on_fullmatch(('网页版', '网页端'))
+async def rhost(bot, ev: CQEvent):
+    await bot.send(ev, Config.host)
+
+
+@sv.on_suffix('在线模式')
+async def setv(bot, ev: CQEvent):
+    if not priv.check_priv(ev, priv.ADMIN):
+        return
+    kw = ev.message.extract_plain_text().strip()
+    if kw in ('开启'):
+        online = True
+    elif kw in ('关闭'):
+        online = False
+    Config.set('online', online)
+    await bot.send(ev, '已'+kw)
+
+
+@sv.on_prefix(('色批信息', '涩批信息'))
+async def spinfo(bot, ev: CQEvent):
+    for _ in ev.message:
+        if _['type'] == 'at' and _['data']['qq'] != 'all':
+            uid = int(_['data']['qq'])
+            info = await Gm(ev).member_info(uid)
+            name = info['card'] or info['nickname']
+            break
+    else:
+        uid = ev.user_id
+        name = ev.sender['card'] or ev.sender['nickname']
+    if uid == ev.self_id:
+        non = Pic.pics['online']
+        noff = Pic.pics['offline']
+        online = '开启✨' if Config.online else '关闭💤'
+        _spinfo = f'在线模式已{online}\n已从服务器获取{non}份涩图\n已从群友处获取{noff}份涩图'
+    else:
+        user = User(uid)
+        uuid = await user.getname
+        gc = user.count('g')
+        uc = user.count('u')
+        sc = user.count('s')
+        _spinfo = f'{name}({uid})：\n网页端用户名：{uuid}\n共叫了{gc}份涩图💦\n上传了{uc}份涩图🎈\n为{sc}份涩图打了分✔'
+    await bot.send(ev, _spinfo)
